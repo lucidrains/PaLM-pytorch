2996069b0c95cd7b377676d99b41fa5d2c8b6fe7
Aniket Maurya <theaniketmaurya@gmail.com>
2022-04-05 15:06:57 +0000

    black code format

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-  name = 'PaLM-pytorch',
-  packages = find_packages(exclude=[]),
-  version = '0.0.10',
-  license='MIT',
-  description = 'PaLM: Scaling Language Modeling with Pathways - Pytorch',
-  author = 'Phil Wang',
-  author_email = 'lucidrains@gmail.com',
-  url = 'https://github.com/lucidrains/PaLM-pytorch',
-  keywords = [
-    'artificial general intelligence',
-    'deep learning',
-    'transformers',
-    'attention mechanism'
-  ],
-  install_requires=[
-    'einops>=0.4',
-    'torch>=1.6',
-  ],
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name="PaLM-pytorch",
+    packages=find_packages(exclude=[]),
+    version="0.0.10",
+    license="MIT",
+    description="PaLM: Scaling Language Modeling with Pathways - Pytorch",
+    author="Phil Wang",
+    author_email="lucidrains@gmail.com",
+    url="https://github.com/lucidrains/PaLM-pytorch",
+    keywords=[
+        "artificial general intelligence",
+        "deep learning",
+        "transformers",
+        "attention mechanism",
+    ],
+    install_requires=[
+        "einops>=0.4",
+        "torch>=1.6",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
